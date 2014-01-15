bd009557d81f8a44a04320187f06dd207dbc36bd
Patrick Fuller <patrickfuller@gmail.com>
2014-01-15 07:52:06 -0500

    Switched to jekyll for HTML preprocessing

--- js/retina-1.1.0.js
+++ js/retina-1.1.0.js
@@ -1,3 +1,12 @@
+/*!
+ * Retina.js v1.1.0
+ *
+ * Copyright 2013 Imulus, LLC
+ * Released under the MIT license
+ *
+ * Retina.js is an open source script that makes it easy to serve
+ * high-resolution images to devices with retina displays.
+ */
 (function() {
 
   var root = (typeof exports == 'undefined' ? window : exports);
@@ -140,3 +149,4 @@
   }
 
 })();
+
